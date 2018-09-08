d2d06243fca2628085c37545ff8a9b185c5f5344
Corey Walsh <matthias61984@hotmail.com>
2018-09-07 23:17:46 -0700

    damage working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,54 @@
 $(document).ready(function() {
 
 // Define chef characters as objects
-    var ramsay = {
-        name: "Chef Ramsay",
+character = [
+    ramsay = {
+        name: "Gordon Ramsay",
         hp: 150,
         baseAttack: 8,
         counterAttack: 12,
-        weapon: "Chef's Knife",
-        pic: "./assets/images/Gordon_Ramsay.png"
-    };
-    
-    var swedish = {
+        pic: "Gordon_Ramsay.png"
+    },
+    swedish = {
         name: "Swedish Chef",
         hp: 80,
-        baseAttack: 12,
+        baseAttack: 10,
         counterAttack: 3,
-        weapon: "Wooden Spoon",
-        pic: "./assets/images/Swedish_Chef.webp"
-    };
-
-    var julia = {
-        name: "Chef Julia",
+        pic: "Swedish_Chef.webp"
+    },
+    julia = {
+        name: "Julia Childs",
         hp: 120,
         baseAttack: 9,
         counterAttack: 15,
-        weapon: "Rolling Pin",
-        pic: "./assets/images/Julia_Childs.jpg"
-    };
-
-    var fieri = {
+        pic: "Julia_Childs.jpg"
+    },
+    fieri = {
         name: "Guy Fieri",
         hp: 175,
         baseAttack: 12,
         counterAttack: 2,
-        weapon: "Hot Sauce",
-        pic: "./assets/images/Guy_Fieri.jpg"
-    };
+        pic: "Guy_Fieri.jpg"
+    },
+];
 
-// Set character pictures using objects
-    $("#char1").attr("src", ramsay.pic);
-    $("#char2").attr("src", swedish.pic);
-    $("#char3").attr("src", julia.pic);
-    $("#char4").attr("src", fieri.pic);
-
-// Set character stats using objects
-    $("#ramsayHP").html("HP: " + ramsay.hp);
-    $("#ramsayWep").html("Favored Weapon: " + ramsay.weapon);
-    $("#swedishHP").html("HP: " + swedish.hp);
-    $("#swedishWep").html("Favored Weapon: " + swedish.weapon);
-    $("#juliaHP").html("HP: " + julia.hp);
-    $("#juliaWep").html("Favored Weapon: " + julia.weapon);
-    $("#fieriHP").html("HP: " + fieri.hp);
-    $("#fieriWep").html("Favored Weapon: " + fieri.weapon);
+// Loop to display characters' pictures and stats from their objects
+for(var i = 0; i < character.length; i++){
+// Create a new div, add class character and id equal to index value from the character array
+    var div = $("<div>").addClass("col-md-2 character sideline").attr("id",i);
+    $("#characters").append(div);
+    div.append($("<img>").attr("src", "assets/images/" + character[i]["pic"]).addClass("characterPic"));
+    div.append($("<h5>").text(character[i]["name"]));
+    div.append($("<p>").html("Health Points: " + character[i]["hp"]))
+    div.append($("<p>").html("Attack Power: " + character[i]["baseAttack"]));
+    div.append($("<p>").html("Counter Attack: " + character[i]["counterAttack"]))
+};
 
 // When one of the four chefs are clicked, that chef is moved to the player div
     $(".sideline").click(function() {
         if ($("#yourChar").hasClass("empty") == true) {
-            $("#yourChar").html(this);
-            $(this).addClass("playerChar");
+            $("#yourChar").replaceWith(this);
+            $(this).addClass("playerChar offset-md-3");
             $(this).removeClass("sideline");
             $("#yourChar").removeClass("empty");
         // Targeting classes for remaining chefs are changed
@@ -70,7 +61,7 @@ $(document).ready(function() {
     // When one of the remaining chefs is clicked, that chef is moved to the enemy div
         $(".enemies").click(function() {
             if ($("#yourEnemy").hasClass("empty") == true) {
-                $("#yourEnemy").html(this);
+                $("#yourEnemy").replaceWith(this);
                 $(this).addClass("enemyChar");
                 $(this).removeClass("enemies");
                 $("#yourEnemy").removeClass("empty");
@@ -82,9 +73,30 @@ $(document).ready(function() {
 
 // When the fight button is clicked, playerChar and enemyChar deal damage to each other
     $("#fightBtn").click(function() {
-        
-
-        //DEFENDER.hp = (DEFENDER.hp - ATTACKER.baseAttack);
-        //$("#DEFENDERHP").html("HP: " + DEFENDER.hp);
+        if (($("#yourChar").hasClass("empty") == false) && ($("#yourEnemy").hasClass("empty") == false)) {
+        // Re-define combatant IDs as indices of the character array
+            var attackerID = $(".playerChar").attr("id");
+            var defenderID = $(".enemyChar").attr("id");
+        // Set combatant hit point and attack power based on IDs
+            var attackerHP = character[attackerID].hp;
+            var defenderHP = character[defenderID].hp;
+            var baseAttack = character[attackerID].baseAttack;
+            var baseCounter = character[defenderID].counterAttack;
+        // Chosen character deals damage, then attack increases
+            defenderHP -= baseAttack;
+            baseAttack += character[attackerID]["baseAttack"];
+        // Enemy deals damage to player's character
+            attackerHP -= baseCounter;
+        // Update objects to reflect new stats
+            character[attackerID].hp = attackerHP;
+            character[defenderID].hp = defenderHP;
+            character[attackerID].baseAttack = baseAttack;
+        // Update corresponding stat cards with new hp
+            console.log(attackerHP);
+            console.log(defenderHP);
+            console.log(baseAttack);
+        } else {
+            $("#battleLog").html("<p>Please choose your character and an enemy to begin!</p>");
+        };
     });
 });
